ab5b9c31de05e3dbc5766cbb7ed7f2838e82e3f7
Jordan Mendler <jordan@thevelozgroup.com>
2019-08-30 16:08:07 -0700

    x

--- clean_up_google_photos_takeout.sh
+++ clean_up_google_photos_takeout.sh
@@ -12,14 +12,14 @@
 #      REVISION:  ---
 #===============================================================================
 
-DIR="~/Downloads/Takeout/Google Photos"
+DIR=~/Downloads/Takeout/Google\ Photos/
 
 # Remove all media files
-for filetype in mp4 jpg mov png gif ; do
-    find "$DIR" -type filetype -iname *.$i -exec rm -f "{}" \; ;
+for filetype in mp4 jpg jpeg mov png gif ; do
+    find "$DIR" -type f -iname \*.$filetype -exec rm -f "{}" \;
 done
 
 # Remove all empty directories
-find "$DIR" -type d -exec rmdir "{}" \;
+find "$DIR" -type d -empty -delete
 
 # FIXME: Combine all JSON metadata to compile locations
