74d501c0722bf3db6eaaf1dc0513a26d028c90ac
Jordan Mendler <jordan@thevelozgroup.com>
2019-08-30 15:42:21 -0700

    merged in Juno json changes

---
+++ clean_up_google_photos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: clean_up_google_photos.sh
+# 
+#         USAGE: ./clean_up_google_photos.sh 
+# 
+#   DESCRIPTION: 
+# 
+#        AUTHOR: YOUR NAME (), 
+#       CREATED: 08/30/2019 15:39
+#      REVISION:  ---
+#===============================================================================
+
+DIR="~/Dowwnloads/Takeout/Google Photos"
+
+# Remove all media files
+for filetype in mp4 jpg mov png ; do
+    find "$DIR" -type filetype -iname *.$i -exec rm -f "{}" \; ;
+done
+
+# Remove all empty directories
+find "$DIR" -type d -exec rmdir "{}" \;
+
+# FIXME: Combine all JSON metadata to compile locations
